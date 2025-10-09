d4e29f351513f5f8f8c49433719e906c1febdd31
Blaise Durkin <53279712+BlaiseDurkin@users.noreply.github.com>
2025-10-09 13:54:37 -0700

    Add files via upload

---
+++ recipes.py
@@ -0,0 +1,27 @@
+#use spider graph to visualize
+
+#East Asia
+EA1 = ['rice', ' shrimp', ' ginger', ' garlic', ' carrot', ' broccoli', ' soy_sauce', ' sesame_oil', 'white_pepper']
+EA2 = ['rice', ' steak', ' ginger', ' garlic', ' bell_pepper', ' broccoli', 'vinegar', ' soy_sauce', ' sesame_oil', 'white_pepper']
+EA3 = ['rice', ' pork', ' ginger', ' garlic', ' cabbage', ' onion', 'carrot', 'soy_sauce', ' sesame_oil', 'white_pepper']
+EA4 = ['rice', ' shrimp', ' ginger', ' garlic', ' carrot', 'bell_pepper', 'walnut', ' soy_sauce', ' sesame_oil', 'white_pepper']
+EA5 = ['pasta', ' shrimp', ' ginger', ' garlic', ' carrot', 'broccoli', 'seaweed', ' soy_sauce', ' sesame_oil', 'white_pepper']
+
+
+
+#South East Asia
+SEA1 = ['rice', 'steak','garlic', 'cilantro', 'peanut', 'soy_sauce', 'vinegar', 'mint', 'bell_pepper']
+SEA2 = ['rice', 'carrot','lettuce', 'sprouts', 'cilantro', 'mint', 'vinegar', 'peanut']
+SEA3 = ['pasta', 'chicken', 'peanut', 'sprouts', 'garlic', 'lime', 'vinegar', 'pepper', 'soy_sauce', 'basil']
+SEA4 = ['rice', 'chicken', 'onion', 'ginger', 'garlic', 'carrot', 'coriander', 'coconut', 'chili_pepper','turmeric','lemongrass', 'basil', 'green_beans'] #red curry
+SEA5 = ['rice', 'ginger', 'garlic', 'lemongrass', 'curry', 'eggplant', 'peas', 'coconut', 'basil', 'white_pepper']#green curry
+SEA6 = ['rice', 'zucchini', 'curry', 'coconut', 'turmeric', 'lemongrass', 'carrot', 'onion', 'coriander', 'cumin', 'white_pepper', 'ginger', 'garlic', 'cilantro', 'lime']#yellow curry
+
+
+#South Asia
+SA1 = ['rice', 'lentils', 'turmeric','cumin','cardamom', 'curry', 'carrot', 'onion']
+
+
+
+#West Asia
+WA1 = ['turmeric', 'cinnamon']
